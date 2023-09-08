765e7a5a38ac596f4692e481edc494b50a979477
EECOLOR <ewestra@kaliber.net>
2023-09-08 15:15:11 +0200

    Remove excess spaces

--- src/components/Flag.js
+++ src/components/Flag.js
@@ -1,7 +1,7 @@
 import Flags from 'country-flag-icons/react/3x2'
 import styles from './Flag.css'
 
-export function   Flag({ country }) {
+export function Flag({ country }) {
   const Flag = Flags[country]
   return Flag && <Flag className={styles.component} />
 }
